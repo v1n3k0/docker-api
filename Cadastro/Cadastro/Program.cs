8480cba723d3a3d05fb0d6abe2ab762b90d4610b
v1n3k0 <v1n1c1u5_1@hotmail.com>
2022-03-21 18:44:25 -0300

    https

--- Cadastro/Cadastro/Program.cs
+++ Cadastro/Cadastro/Program.cs
@@ -14,6 +14,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseHttpsRedirection();
+
 var summaries = new[]
 {
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
