b62afc5654b9071f80e5943b9a6057e1d76f00d0
v1n3k0 <v1n1c1u5_1@hotmail.com>
2022-03-20 22:54:38 -0300

    debug

--- Cadastro/Cadastro/Program.cs
+++ Cadastro/Cadastro/Program.cs
@@ -1,10 +1,18 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 var summaries = new[]
 {
@@ -22,7 +30,8 @@ app.MapGet("/weatherforecast", () =>
        ))
         .ToArray();
     return forecast;
-});
+})
+.WithName("GetWeatherForecast");
 
 app.Run();
 
